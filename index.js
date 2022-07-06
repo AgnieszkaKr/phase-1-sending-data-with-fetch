b2ed5fed82e7077797eaea23c1020c847f598e59
Agnes <agsawicka@gmail.com>
2022-07-06 10:20:26 -0400

    commit tree 18141a98d0da45e49dd8f357d2cca527af44a455

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+const submitData = (name, email) =>{
+    return fetch('http://localhost:3000/users', 
+    {
+        method: 'POST', 
+        headers:{
+            'Content-Type':'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            'name':name,
+            'email':email
+        })
+    }).then(res => res.json())
+    .then(obj => {
+        document.body.append(document.createElement('p').textContent = obj.id)
+    }).catch(function(message){
+        document.body.append(document.createElement('p').textContent = message)
+
+    })
+
+}
+
+submitData("John", "john@gmail.com")
